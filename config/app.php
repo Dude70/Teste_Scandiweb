e28dba48a5ccb1f2887174e59954ed72f48a0dff
Dude70 <dudupinheiro014@gmail.com>
2023-04-25 17:10:49 -0300

    commit

---
+++ config/app.php
@@ -0,0 +1,7 @@
+<?php
+# Database Configuration
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_NAME', 'id20653304_products');
+define('DB_USER', 'id20653304_dude70');
+define('DB_PASS', 'eB6Ks((p3h%_#v@1');
